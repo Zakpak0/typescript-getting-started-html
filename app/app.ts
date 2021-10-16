5b5d4ffd93c251ff758f60f1e39242a6a19ec499
Zakpak0 <ZakharyOliver808@gmail.com>
2021-10-15 19:33:11 -1000

    first commit

---
+++ app/app.ts
@@ -0,0 +1,57 @@
+/// <reference path="player.ts" />
+
+
+function startGame() {
+    // starting a new game
+
+    let playerName: string | undefined = getInputValue('playername');
+    logPlayer(playerName)
+
+    postScore(80, playerName)
+    postScore(-5, playerName);
+    
+    // var messagesElement = document.getElementById('messages');
+    // You can assert that an object is non null by added an ! after the object
+    // messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...'
+}
+
+function logPlayer(name: string = 'MultiMath Player'): void {
+    console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementID: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+    if (inputElement.value === '') {
+        return undefined;
+    }else {
+        return inputElement.value;
+    }
+}
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+
+    let logger: (value: string) => void;
+
+    if (score < 0){
+        logger = logError;
+    }else{
+        logger = logMessage;
+    }
+
+    const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+
+    logger(`Score: ${score}`);
+}
+
+document.getElementById('startGame')!.addEventListener('click', startGame);
+
+const logMessage = (message: string) => {
+    console.log(message);
+}
+function logError(err: string): void {
+    console.error(err)
+}
+const firstPlayer: Player = new Player();
+firstPlayer.name = 'Lanier'
+console.log(firstPlayer.formatName())
